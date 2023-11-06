75dc61b433a445fc8c6f3dbc7d28124cb704e082
ofc <ofc@gov.cn>
2023-11-06 19:27:21 +0800

    update

--- cmark.py
+++ cmark.py
@@ -1,14 +1,16 @@
 import sys
-from PyQt5.QtQml import QQmlApplicationEngine
-from PyQt5.QtGui import QGuiApplication
-from PyQt5.QtCore import QObject
+from PySide6.QtQml import QQmlApplicationEngine
+from PySide6.QtGui import QGuiApplication
+from PySide6.QtCore import QObject
 
-if __name__=="__main__":
-    app=QGuiApplication(sys.argv)
+if __name__ == "__main__":
+    app = QGuiApplication(sys.argv)
 
-    engine=QQmlApplicationEngine()
+    engine = QQmlApplicationEngine()
     engine.load("main.qml")
-    root = engine.rootObjects()[0].findChild(QObject, "textBrowser")
-    root.setProperty("text", "Text Browser")
+    root = engine.rootObjects()[0]
+    win = root.findChild(QObject, "textBrowser")
+    win.setProperty("text", "Text Browser")
+    win.append("Append")
     
     sys.exit(app.exec())
